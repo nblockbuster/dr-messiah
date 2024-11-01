d7c71eb122c7b1f8f3400743ac2fca8da69bdde7
nblockbuster <34419030+nblockbuster@users.noreply.github.com>
2024-11-01 12:20:19 -0400

    fix index buffer stride

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,7 @@ mod mpk;
 
 use binrw::BinReaderExt;
 use clap::Parser;
-use model::Vertex;
 use mpk::{MpkInfo, ResourcesMpkInfo};
-use porter_cast::{CastFile, CastId, CastNode, CastPropertyId};
-use porter_math::{Vector2, Vector3};
 use std::fs::File;
 use std::io::{Read, Seek, Write};
 use std::path::PathBuf;
@@ -28,7 +25,7 @@ struct Args {
     #[arg(short)]
     etsb_path: Option<String>,
 
-    /// Convert model file to <collada?>
+    /// Convert model file to cast file
     #[arg(short)]
     model_path: Option<String>,
 }
@@ -52,204 +49,7 @@ fn main() -> anyhow::Result<()> {
     println!("output_path: {:#?}", output_path);
 
     if let Some(model_path) = args.model_path {
-        let mut mfile = File::open(model_path.clone())?;
-        let model: model::ModelHeader = mfile.read_le()?;
-        println!("{:?}", model);
-
-        // check all buffer layouts for indices buffer
-        let mut indices_buffer = None;
-        for layout in &model.buffer_layouts {
-            let buffer = model::get_buffers_from_layout(&layout.data);
-            for b in buffer.clone() {
-                if b.buffer_type == model::BufferType::Indices {
-                    indices_buffer = Some(b);
-                    break;
-                }
-            }
-            if indices_buffer.is_some() {
-                break;
-            }
-        }
-
-        // default u16, if buffer exists then use buffer size
-        let mut indices: Vec<Vec<u32>> = Vec::new();
-        for _ in 0..model.index_count / 3 {
-            let mut index = Vec::new();
-            for _ in 0..3 {
-                if let Some(indices_buffer) = indices_buffer {
-                    match indices_buffer.size {
-                        0x4 => {
-                            index.push(mfile.read_le::<u32>()?);
-                        }
-                        _ => {
-                            unimplemented!();
-                        }
-                    }
-                } else {
-                    index.push(mfile.read_le::<u16>()? as u32);
-                }
-            }
-
-            indices.push(index);
-        }
-        println!("{:?}", indices.len());
-        println!("{:?}", indices.last());
-
-        let mut vertices: Vec<Vertex> = vec![Vertex::default(); model.vertex_count as usize];
-        for layout in &model.buffer_layouts {
-            let buffer = model::get_buffers_from_layout(&layout.data);
-            if buffer.is_empty() {
-                continue;
-            }
-            if buffer
-                .iter()
-                .any(|b| b.buffer_type == model::BufferType::Indices)
-            {
-                continue; // covered earlier
-            }
-            if buffer
-                .iter()
-                .any(|b| b.buffer_type == model::BufferType::Binormal)
-            {
-                continue; // todo: implement binormals/tangent
-            }
-            println!("{:?}", buffer);
-            for vid in 0..model.vertex_count {
-                let mut vertex = Vertex::default();
-                for b in &buffer {
-                    let mut data = Vec::new();
-                    for _ in 0..b.size {
-                        match b.buffer_format {
-                            model::BufferFormat::Float => {
-                                data.push(mfile.read_le::<f32>()?);
-                            }
-                            model::BufferFormat::Byte => {
-                                data.push(mfile.read_le::<u8>()? as f32 / 255.0);
-                            }
-                            model::BufferFormat::Half => {
-                                data.push(mfile.read_le::<u16>()? as f32 / 65535.0);
-                            }
-                            // model::BufferFormat::Half => {
-                            //     data.push(half::f16::from_bits(mfile.read_le::<u16>()?).to_f32());
-                            // }
-                            _ => {
-                                unimplemented!();
-                            }
-                        }
-                        // println!("{:?}", data);
-                    }
-                    match b.buffer_type {
-                        model::BufferType::Position => {
-                            vertex.position = [data[0], data[1], data[2]];
-                        }
-                        model::BufferType::Normal => {
-                            vertex.normal = [data[0], data[1], data[2]];
-                        }
-                        model::BufferType::TexcoordTangent => {
-                            vertex.texcoord.push([data[0], data[1]]);
-                        }
-                        model::BufferType::Tangent => {
-                            vertex.tangent = [data[0], data[1], data[2], data[3]];
-                        }
-                        model::BufferType::Binormal => {
-                            vertex.binormal = [data[0], data[1], data[2], data[3]];
-                        }
-                        model::BufferType::Color => {
-                            vertex.color = [data[0], data[1], data[2], data[3]];
-                        }
-                        model::BufferType::Weights => {
-                            vertex.weights = [data[0], data[1], data[2], data[3]];
-                        }
-                        _ => {}
-                    }
-                }
-                vertices[vid as usize] = vertex;
-            }
-        }
-        println!("{:?}", vertices.len());
-        println!("{:?}", vertices.first());
-        println!("{:?}", vertices.last());
-
-        let uv_layer_count: u32 =
-            vertices.iter().map(|v| v.texcoord.len()).max().unwrap_or(0) as u32;
-
-        let mut castfile = CastFile::new();
-        let mut root = CastNode::root();
-        let mesh = root.create(CastId::Model).create(CastId::Mesh);
-
-        let uvlayers = mesh.create_property(CastPropertyId::Integer32, "ul");
-        uvlayers.push(uv_layer_count);
-
-        let colorlayers = mesh.create_property(CastPropertyId::Integer32, "cl");
-        colorlayers.push(1_u32);
-
-        let pos = mesh.create_property(CastPropertyId::Vector3, "vp");
-        for vertex in &vertices {
-            pos.push(Vector3::new(
-                vertex.position[0],
-                vertex.position[1],
-                vertex.position[2],
-            ));
-        }
-
-        let norm = mesh.create_property(CastPropertyId::Vector3, "vn");
-        for vertex in &vertices {
-            norm.push(Vector3::new(
-                vertex.normal[0],
-                vertex.normal[1],
-                vertex.normal[2],
-            ));
-        }
-
-        let color = mesh.create_property(CastPropertyId::Integer32, "c0");
-        for vertex in &vertices {
-            let col: u32 = ((vertex.color[0] * 255.0) as u32) << 24
-                | ((vertex.color[1] * 255.0) as u32) << 16
-                | ((vertex.color[2] * 255.0) as u32) << 8
-                | ((vertex.color[3] * 255.0) as u32);
-            color.push(col);
-        }
-
-        for i in 0..uv_layer_count {
-            let uv = mesh.create_property(CastPropertyId::Vector2, format!("u{}", i));
-            for vertex in &vertices {
-                if i < vertex.texcoord.len() as u32 {
-                    uv.push(Vector2::new(
-                        vertex.texcoord[i as usize][0],
-                        vertex.texcoord[i as usize][1],
-                    ));
-                } else {
-                    uv.push(Vector2::new(0.0, 0.0));
-                }
-            }
-        }
-
-        // let binormal = mesh.create_property(CastPropertyId::Vector3, "vt");
-        // for vertex in &vertices {
-        //     binormal.push(Vector3::new(
-        //         vertex.tangent[0],
-        //         vertex.tangent[1],
-        //         vertex.tangent[2],
-        //     ));
-        // }
-
-        // let uv = mesh.create_property(CastPropertyId::Vector2, "u0");
-        // for vertex in &vertices {
-        //     uv.push(Vector2::new(vertex.texcoord[0], vertex.texcoord[1]));
-        // }
-
-        let mut idx = mesh.create_property(CastPropertyId::Integer32, "f");
-        for index in indices.iter() {
-            idx = idx.push(index[0]);
-            idx = idx.push(index[1]);
-            idx = idx.push(index[2]);
-        }
-
-        castfile.push(root);
-
-        let mut cast_out = File::create(PathBuf::from(model_path).with_extension("cast"))?;
-        castfile.write(&mut cast_out)?;
-
+        model::export_model(&model_path)?;
         return Ok(());
     }
 
@@ -317,31 +117,23 @@ fn main() -> anyhow::Result<()> {
 
                     mpk_file.seek(std::io::SeekFrom::Start(record.mpk_offset as u64))?;
                     mpk_file.read_exact(&mut data)?;
-                    // if data.len() > 0x4 {
-                    //     println!("{:X?} ({})", &data[0x0..0x4], String::from_utf8_lossy(&data[0x0..0x4]));
-                    // }
                 }
                 if file_path.extension().is_none() {
                     file_path.set_extension("bin");
                 }
-                // if file_path.extension().unwrap() == "4" {
-                //     file_path.set_extension("tex");
-                // }
-                if data.len() > 0x4 {
-                    if let Some(compression_type) = compression::get_compression_type(&data[0x0..])
-                    {
-                        data = compression::decompress(compression_type, &data[0x0..])?;
-                    }
+                if data.len() > 0x4
+                    && let Some(compression_type) = compression::get_compression_type(&data[0x0..])
+                {
+                    data = compression::decompress(compression_type, &data[0x0..])?;
                 }
-                if data.len() > 0x38 {
-                    if let Some(compression_type) = compression::get_compression_type(&data[0x38..])
+                if data.len() > 0x38
+                    && let Some(compression_type) = compression::get_compression_type(&data[0x38..])
+                {
+                    if let Ok(extra_decomp_data) =
+                        compression::decompress(compression_type, &data[0x38..])
                     {
-                        if let Ok(extra_decomp_data) =
-                            compression::decompress(compression_type, &data[0x38..])
-                        {
-                            data.truncate(0x38);
-                            data.extend_from_slice(&extra_decomp_data);
-                        }
+                        data.truncate(0x38);
+                        data.extend_from_slice(&extra_decomp_data);
                     }
                 }
                 std::fs::create_dir_all(file_path.parent().unwrap())?;
@@ -363,7 +155,7 @@ fn main() -> anyhow::Result<()> {
     let mpk_file = File::open(mpkinfo_path.clone().with_extension("mpk"))?;
     let mpk_file = Mutex::new(mpk_file);
 
-    let start = std::time::Instant::now();
+    // let start = std::time::Instant::now();
 
     mpkinfo_vec
         .iter_mut()
@@ -385,14 +177,14 @@ fn main() -> anyhow::Result<()> {
             {
                 data = compression::decompress(compression_type, &data[0x0..])?;
             }
-            if data.len() > 0x38 {
-                if let Some(compression_type) = compression::get_compression_type(&data[0x38..]) {
-                    if let Ok(extra_decomp_data) =
-                        compression::decompress(compression_type, &data[0x38..])
-                    {
-                        data.truncate(0x38);
-                        data.extend_from_slice(&extra_decomp_data);
-                    }
+            if data.len() > 0x38
+                && let Some(compression_type) = compression::get_compression_type(&data[0x38..])
+            {
+                if let Ok(extra_decomp_data) =
+                    compression::decompress(compression_type, &data[0x38..])
+                {
+                    data.truncate(0x38);
+                    data.extend_from_slice(&extra_decomp_data);
                 }
             }
             std::fs::create_dir_all(file_path.parent().unwrap())?;
@@ -401,6 +193,6 @@ fn main() -> anyhow::Result<()> {
             Ok(())
         })?;
 
-    println!("Elapsed: {:?}", start.elapsed());
+    // println!("Elapsed: {:?}", start.elapsed());
     Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,12 @@
-use binrw::{binread, BinRead};
+use std::{fs::File, path::PathBuf};
+
+use anyhow::{Result, Error};
+use binrw::{binread, BinRead, BinReaderExt};
+use porter_cast::{CastFile, CastId, CastNode, CastPropertyId};
+use porter_math::{Vector2, Vector3};
 
 #[derive(BinRead, Debug, Clone)]
-#[br(magic=b".MESSIAH")]
+#[br(magic = b".MESSIAH")]
 pub struct ModelHeader {
     pub unk8: u32,
     pub unkc: u32,
@@ -11,7 +16,7 @@ pub struct ModelHeader {
     #[br(count = 4)]
     pub buffer_layouts: Vec<BufferLayout>,
     pub unk_floats: [f32; 10],
-    pub unk2: [u32;4],
+    pub unk2: [u32; 4],
 }
 
 #[binread]
@@ -19,7 +24,7 @@ pub struct ModelHeader {
 pub struct BufferLayout {
     #[br(temp)]
     len: u16,
-    
+
     #[br(map = |s: Vec<u8>| String::from_utf8_lossy(&s).to_string(), count = len)]
     pub data: String,
 }
@@ -28,21 +33,19 @@ pub struct BufferLayout {
 pub enum BufferType {
     Position,
     Normal,
-    // TODO: find out how to separate from texcoord (both 'T', maybe check if earlier layout has 'T' as well as position/color/normal)
-    TexcoordTangent,
+    Color,
+    Texcoord,
+    BlendWeight,
+    BlendIndices,
     Tangent,
     Binormal,
-    Color,
-    Weights,
-    Indices,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum BufferFormat {
     Float,
     Byte,
-    Short,
-    Half
+    Half,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -55,18 +58,64 @@ pub struct Buffer {
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct Vertex {
     pub position: [f32; 3],
-    pub normal: [f32; 3],
-    // multiple texcoord layers
-    pub texcoord: Vec<[f32; 2]>,
+    pub normal: [f32; 4],
+    pub color: [f32; 4],
+    pub texcoords: Vec<[f32; 2]>,
+    pub blend_weight: [f32; 4],
+    pub blend_indices: [u32; 4],
     pub tangent: [f32; 4],
     pub binormal: [f32; 4],
-    pub color: [f32; 4],
-    pub weights: [f32; 4],
 }
 
+impl Vertex {
+    pub fn combine(&self, other: &Vertex) -> Vertex {
+        let mut new = Vertex::default();
+        if self.position != [0.0; 3] {
+            new.position = self.position;
+        } else {
+            new.position = other.position;
+        }
+        if self.normal != [0.0; 4] {
+            new.normal = self.normal;
+        } else {
+            new.normal = other.normal;
+        }
+        if self.color != [0.0; 4] {
+            new.color = self.color;
+        } else {
+            new.color = other.color;
+        }
+        if !self.texcoords.is_empty() {
+            new.texcoords = self.texcoords.clone();
+        } else {
+            new.texcoords = other.texcoords.clone();
+        }
+        if self.blend_weight != [0.0; 4] {
+            new.blend_weight = self.blend_weight;
+        } else {
+            new.blend_weight = other.blend_weight;
+        }
+        if self.blend_indices != [0; 4] {
+            new.blend_indices = self.blend_indices;
+        } else {
+            new.blend_indices = other.blend_indices;
+        }
+        if self.tangent != [0.0; 4] {
+            new.tangent = self.tangent;
+        } else {
+            new.tangent = other.tangent;
+        }
+        if self.binormal != [0.0; 4] {
+            new.binormal = self.binormal;
+        } else {
+            new.binormal = other.binormal;
+        }
+        new
+    }
+}
 
-// P3F_N4B_T2F_T2F - Posion 3 Float, Normal 4 Byte, Texcoord 2 Float, Texcoord 2 Float?
-// T4H_B4H - Tangent 4 Half, Bitangent 4 Half?
+// P3F_N4B_T2F_T2F - Posion 3 Float, Normal 4 Byte, Texcoord 2 Float, Texcoord 2 Float
+// T4H_B4H - Tangent 4 Half, Binormal 4 Half
 
 pub fn get_buffers_from_layout(data: &str) -> Vec<Buffer> {
     if data == "None" {
@@ -79,11 +128,11 @@ pub fn get_buffers_from_layout(data: &str) -> Vec<Buffer> {
         let buffer_type = match b.chars().next().unwrap() {
             'P' => BufferType::Position,
             'N' => BufferType::Normal,
-            'T' => BufferType::TexcoordTangent,
-            'B' => BufferType::Binormal,
             'C' => BufferType::Color,
-            'W' => BufferType::Weights,
-            'I' => BufferType::Indices,
+            'T' => BufferType::Texcoord, // and Tangent
+            'W' => BufferType::BlendWeight,
+            'I' => BufferType::BlendIndices,
+            'B' => BufferType::Binormal,
             _ => panic!("Unknown buffer type: {}", data),
         };
 
@@ -92,7 +141,6 @@ pub fn get_buffers_from_layout(data: &str) -> Vec<Buffer> {
         let buffer_format = match b.chars().nth(2).unwrap() {
             'F' => BufferFormat::Float,
             'B' => BufferFormat::Byte,
-            'S' => BufferFormat::Short,
             'H' => BufferFormat::Half,
             _ => panic!("Unknown buffer format: {}", data),
         };
@@ -105,4 +153,200 @@ pub fn get_buffers_from_layout(data: &str) -> Vec<Buffer> {
     }
 
     buffers
+}
+
+pub fn export_model(model_path: &str) -> Result<(), Error> {
+    let mut mfile = File::open(model_path.clone())?;
+    let model: ModelHeader = mfile.read_le()?;
+    println!("{:?}", model);
+
+    let mut indices: Vec<Vec<u32>> = Vec::new();
+    let index_stride = if model.index_count > 0xFFFF { 4 } else { 2 };
+    for _ in 0..model.index_count / 3 {
+        let mut index = Vec::new();
+        for _ in 0..3 {
+            match index_stride {
+                2 => {
+                    index.push(mfile.read_le::<u16>()? as u32);
+                }
+                4 => {
+                    index.push(mfile.read_le::<u32>()?);
+                }
+                _ => {
+                    unimplemented!();
+                }
+            }
+        }
+        indices.push(index);
+    }
+
+    let mut vertices: Vec<Vertex> = vec![Vertex::default(); model.vertex_count as usize];
+    for layout in &model.buffer_layouts {
+        let buffer = get_buffers_from_layout(&layout.data);
+        if buffer.is_empty() {
+            continue;
+        }
+
+        let t_is_tangent = !(buffer.iter().any(|b| b.buffer_type == BufferType::Texcoord)
+            && buffer.iter().any(|b| b.buffer_type == BufferType::Position));
+
+        println!("{:?}", buffer);
+        for vid in 0..model.vertex_count {
+            let mut vertex = Vertex::default();
+            for b in &buffer {
+                let mut data = Vec::new();
+                //println!("{:?}", b.buffer_type);
+                for _ in 0..b.size {
+                    match b.buffer_format {
+                        BufferFormat::Float => {
+                            data.push(mfile.read_le::<f32>()?);
+                        }
+                        BufferFormat::Byte => {
+                            data.push(mfile.read_le::<u8>()? as f32);
+                        }
+                        BufferFormat::Half => {
+                            data.push(mfile.read_le::<u16>()? as f32 / 65535.0);
+                        }
+                        _ => {
+                            unimplemented!();
+                        }
+                    }
+                    //println!("{:?}", data);
+                }
+                match b.buffer_type {
+                    BufferType::Position => {
+                        vertex.position = [data[0], data[1], data[2]];
+                    }
+                    BufferType::Normal => {
+                        vertex.normal = [data[0], data[1], data[2], data[3]];
+                    }
+                    BufferType::Color => {
+                        vertex.color = [data[0], data[1], data[2], data[3]];
+                    }
+                    BufferType::Texcoord => {
+                        if t_is_tangent {
+                            vertex.tangent = [data[0], data[1], data[2], data[3]];
+                        } else {
+                            vertex.texcoords.push([data[0], data[1]]);
+                        }
+                    }
+                    BufferType::BlendWeight => {
+                        vertex.blend_weight = [data[0], data[1], data[2], data[3]];
+                    }
+                    BufferType::BlendIndices => {
+                        vertex.blend_indices = [
+                            data[0] as u32,
+                            data[1] as u32,
+                            data[2] as u32,
+                            data[3] as u32,
+                        ];
+                    }
+                    BufferType::Tangent => {
+                        vertex.tangent = [data[0], data[1], data[2], data[3]];
+                    }
+                    BufferType::Binormal => {
+                        vertex.binormal = [data[0], data[1], data[2], data[3]];
+                    }
+                    _ => unimplemented!("{:?}", b.buffer_type),
+                }
+            }
+            vertices[vid as usize] = vertices[vid as usize].combine(&vertex);
+        }
+    }
+    println!("{:?}", vertices.len());
+    println!("{:?}", vertices.first());
+    println!("{:?}", vertices.last());
+
+    let uv_layer_count: u32 = vertices
+        .iter()
+        .map(|v| v.texcoords.len())
+        .max()
+        .unwrap_or(0) as u32;
+
+    let mut castfile = CastFile::new();
+    let mut root = CastNode::root();
+    let mesh = root.create(CastId::Model).create(CastId::Mesh);
+
+    let uvlayers = mesh.create_property(CastPropertyId::Integer32, "ul");
+    uvlayers.push(uv_layer_count);
+
+    let colorlayers = mesh.create_property(CastPropertyId::Integer32, "cl");
+    colorlayers.push(1_u32);
+
+    let pos = mesh.create_property(CastPropertyId::Vector3, "vp");
+    for vertex in &vertices {
+        pos.push(Vector3::new(
+            vertex.position[0],
+            vertex.position[1],
+            vertex.position[2],
+        ));
+    }
+
+    let norm = mesh.create_property(CastPropertyId::Vector3, "vn");
+    for vertex in &vertices {
+        norm.push(Vector3::new(
+            vertex.normal[0],
+            vertex.normal[1],
+            vertex.normal[2],
+        ));
+    }
+
+    let color = mesh.create_property(CastPropertyId::Integer32, "c0");
+    for vertex in &vertices {
+        let col: u32 = ((vertex.color[0] * 255.0) as u32) << 24
+            | ((vertex.color[1] * 255.0) as u32) << 16
+            | ((vertex.color[2] * 255.0) as u32) << 8
+            | ((vertex.color[3] * 255.0) as u32);
+        color.push(col);
+    }
+
+    for i in 0..uv_layer_count {
+        let uv = mesh.create_property(CastPropertyId::Vector2, format!("u{}", i));
+        for vertex in &vertices {
+            if i < vertex.texcoords.len() as u32 {
+                uv.push(Vector2::new(
+                    vertex.texcoords[i as usize][0],
+                    vertex.texcoords[i as usize][1],
+                ));
+            } else {
+                uv.push(Vector2::new(0.0, 0.0));
+            }
+        }
+    }
+
+    let weights = mesh.create_property(CastPropertyId::Float, "wv");
+    for vertex in &vertices {
+        weights.push(vertex.blend_weight[0]);
+        weights.push(vertex.blend_weight[1]);
+        weights.push(vertex.blend_weight[2]);
+        weights.push(vertex.blend_weight[3]);
+    }
+
+    let bindices = mesh.create_property(CastPropertyId::Integer32, "wb");
+    for vertex in &vertices {
+        bindices.push(vertex.blend_indices[0]);
+        bindices.push(vertex.blend_indices[1]);
+        bindices.push(vertex.blend_indices[2]);
+        bindices.push(vertex.blend_indices[3]);
+    }
+
+    // let tangent = mesh.create_property(CastPropertyId::Vector3, "vt");
+    // for vertex in &vertices {
+    //     tangent.push(Vector3::new(
+    //         vertex.tangent[0],
+    //         vertex.tangent[1],
+    //         vertex.tangent[2],
+    //     ));
+    // }
+
+    let idx = mesh.create_property(CastPropertyId::Integer32, "f");
+    indices.iter().flatten().for_each(|i| {
+        idx.push(*i);
+    });
+
+    castfile.push(root);
+
+    let mut cast_out = File::create(PathBuf::from(model_path).with_extension("cast"))?;
+    castfile.write(&mut cast_out)?;
+    Ok(())
 }
